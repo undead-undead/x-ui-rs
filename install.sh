24e38f777069d6e64fa4257b9b8b75f76aca88ba
undead-undead <biubiuboy1200372@gmail.com>
2026-01-03 11:42:37 +0800

    chore: release v0.1.5

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ XRAY_BIN_PATH="$INSTALL_PATH/bin/xray"
 ENV_FILE="$INSTALL_PATH/.env"
 SERVICE_FILE="/etc/systemd/system/x-ui.service"
 
-RELEASE_URL="https://github.com/undead-undead/x-ui-rs/releases/download/v0.1.4/x-ui-linux-${arch}.tar.gz"
+RELEASE_URL="https://github.com/undead-undead/x-ui-rs/releases/download/v0.1.5/x-ui-linux-${arch}.tar.gz"
 
 install_dependencies() {
     if [[ -f /usr/bin/apt ]]; then

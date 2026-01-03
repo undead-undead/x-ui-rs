faf5dc1aa826962099cc70b1addf58f756fedadf
undead-undead <biubiuboy1200372@gmail.com>
2026-01-03 18:16:49 +0800

    fix(backend): subpath routing and root redirect (v1.1.28)

--- install.sh
+++ install.sh
@@ -110,7 +110,7 @@ XRAY_BIN_PATH="$INSTALL_PATH/bin/xray"
 ENV_FILE="$INSTALL_PATH/.env"
 SERVICE_FILE="/etc/systemd/system/x-ui.service"
 
-RELEASE_URL="https://github.com/undead-undead/x-ui-rs/releases/download/v1.1.27/x-ui-linux-${arch}.tar.gz"
+RELEASE_URL="https://github.com/undead-undead/x-ui-rs/releases/download/v1.1.28/x-ui-linux-${arch}.tar.gz"
 
 install_dependencies() {
     i18n "install_deps"
